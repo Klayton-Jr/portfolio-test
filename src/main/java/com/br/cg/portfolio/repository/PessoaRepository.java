8bad423692466f65503ca3c440377f1c375497b7
Klayton Junior <klaytonjunior@hotmail.com>
2023-12-02 23:01:22 -0300

    Delete src directory

--- src/main/java/com/br/cg/portfolio/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.cg.portfolio.repository;
-
-import com.br.cg.portfolio.domain.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
